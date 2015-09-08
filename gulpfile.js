ae7af243214c44b5ea79e8266f4f523b77aa91c1
homm <homm86@gmail.com>
2015-09-08 14:15:03 +0300

    add gulp-require-file (not suit at first glance)

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
+var requireFile = require('gulp-require-file');
 
 gulp.task('coffee', function() {
   gulp.src('./src/javascripts/**/*.coffee')
-    .pipe(coffee({bare: true}).on('error', gutil.log))
-    .pipe(gulp.dest('./dst/'))
+    .pipe(
+      coffee({
+        bare: true
+      })
+        .on('error', gutil.log)
+    )
+    .pipe(gulp.dest('./dst/javascripts/'));
+});
+
+
+gulp.task("scripts", function() {
+    return gulp.src('./src/javascripts/widget.coffee')
+        .pipe(requireFile({}))
+        .pipe(gulp.dest("./dst/"));
 });
